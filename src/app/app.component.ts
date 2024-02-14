b68ce23b1d0df11ab437767d8e7de649f97e53b8
Felipe <felipe@visionar.io>
2024-02-14 19:17:11 -0300

    Layout banner mosaic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BannerSplitComponent } from './banners/banner-split/banner-split.component';
+import { BannerMosaicComponent } from './banners/banner-mosaic/banner-mosaic.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BannerSplitComponent],
+  imports: [RouterOutlet, BannerSplitComponent, BannerMosaicComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/banners/banner-mosaic/banner-mosaic.component.ts
+++ src/app/banners/banner-mosaic/banner-mosaic.component.ts
@@ -1,24 +1,66 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-banner-mosaic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './banner-mosaic.component.html',
   styleUrl: './banner-mosaic.component.scss'
 })
 export class BannerMosaicComponent {
 
-  @Input() items: [] | any | undefined;
-	public colors;
+  @Input() items: [{ 'featured': boolean, 'link': string, 'category': string, 'collor': string, 'title': string, 'description': string, 'image': string }] | any | undefined;
 
-	constructor() {
-	  this.colors = colorsCategory;
-	 }
-
-	ngOnInit() {
-		console.debug(this.items);
-	}
+  ngOnInit() {
+    this.items = [
+      {
+        "featured": true,
+        "link": "/componentes",
+        "category": "Test",
+        "collor": "#003366",
+        "title": "Título do item um",
+        "description": "Description text",
+        "image": "assets/images/070-550x520.jpg"
+      },
+      {
+        "featured": false,
+        "link": "/componentes",
+        "category": "Test",
+        "collor": "#006666",
+        "title": "Título do item dois",
+        "description": "Description text",
+        "image": "assets/images/024-550x520.jpg"
+      },
+      {
+        "featured": false,
+        "link": "/componentes",
+        "category": "Test",
+        "collor": "#424242",
+        "title": "Título do item três",
+        "description": "Description text",
+        "image": "assets/images/092-550x520.jpg"
+      },
+      {
+        "featured": false,
+        "link": "/componentes",
+        "category": "Test",
+        "collor": "#cc0000",
+        "title": "Título do item quatro",
+        "description": "Description text",
+        "image": "assets/images/018-550x520.jpg"
+      },
+      {
+        "featured": false,
+        "link": "/componentes",
+        "category": "Test",
+        "collor": "#cccc00",
+        "title": "Título do item cinco",
+        "description": "Description text",
+        "image": "assets/images/120-768x512.jpg"
+      },
+    ]
+  }
 
 }
